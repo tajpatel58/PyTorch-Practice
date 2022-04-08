016298a4cbf0aeea88ac41cc73b9730819906480
tajpatel58 <tajpatel58@gmail.com>
2022-04-09 00:13:08 +0100

    Adding Transforms and Model Hyperparams

--- Transfer_Learning.py
+++ Transfer_Learning.py
@@ -7,3 +7,33 @@ from torch.optim import SGD
 from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, models, transforms
+import numpy as np
+
+# Hyperparameters: 
+number_epochs = 5
+batch_size = 5
+mean = np.array([0.5, 0.5, 0.5])
+std = np.array([0.25, 0.25, 0.25])
+
+# Transforms: 
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+}
+
+
+# Initialise datasets and dataloaders:
+data = '<ENTER NAME OF FOLDER FOR DATA>'
+data_path = f'./Transfer_Learning_Data/{data}/'
+phases = ['Training', 'Testing']
+
