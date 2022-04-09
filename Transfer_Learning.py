8790549fccd0b55a9bad71191d32dfd73e88023d
tajpatel58 <tajpatel58@gmail.com>
2022-04-09 23:59:01 +0100

    Starting Training Loop

--- Transfer_Learning.py
+++ Transfer_Learning.py
@@ -8,6 +8,8 @@ from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, models, transforms
 import numpy as np
+import copy 
+import os
 
 # Hyperparameters: 
 number_epochs = 5
@@ -46,3 +48,40 @@ datasets_dict = {phase : datasets.ImageFolder(f'{data_path}{phase}',
 dataloaders = {phase : DataLoader(datasets_dict[phase], shuffle=True, 
                                   batch_size=batch_size)
                                   for phase in phases}
+
+#Defining a function to train our model: 
+def train_model(model, optimizer, num_epochs, lr_scheduler, loss_func):
+    """
+    A function which applies the training loop for us. 
+
+
+    Parameters:
+    model : (torch.nn.Module) - Initial Deep Learning Model
+    optimizer : (optimizer) - Iterative process used to optimize the loss function. 
+    num_epochs : (int) - Number of epochs to be used in training. 
+    lr_scheduler : Learning Rate optimiser. 
+    loss_func : Loss function used for model. 
+    
+    Returns:
+    model : (torch.nn.Module) - Optimized Deep Learning Model
+    """
+
+    best_model = copy.deepcopy(model.state_dict())
+    best_testing_acc = 0
+
+    for epoch in range(num_epochs):
+        error = 0
+        # Save the state of the model
+        model_state = copy.deepcopy(model.state_dict())
+
+        for phase in phases:
+            if phase == 'Training':
+                # Set Model to training mode. (This accounts for dropout layers etc)
+                model.train()
+            else:
+                # Set model to testing/validation mode. 
+                model.eval() 
+
+            for i, (data, labels) in enumerate(dataloaders[phase]):
+                predictions = model(data)
+                
